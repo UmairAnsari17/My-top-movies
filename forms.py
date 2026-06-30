2b343d3d3d80b927f539cd6a243cf390edb43dea
UmairAnsari17 <155252236+UmairAnsari17@users.noreply.github.com>
2026-06-30 23:53:51 +0530

    Add authentication features and improve application UI

---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+
+db = SQLAlchemy()
+
+
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(200))
+
+    film_data = db.relationship('Movie', backref='user')
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, request
+import re
+from flask import Flask, render_template, redirect, url_for, request, abort, flash
 from flask_bootstrap import Bootstrap5
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.numeric import FloatField
+from wtforms.validators import DataRequired, NumberRange
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import db, User, Movie
 import requests
 import os
 
@@ -22,10 +25,10 @@ Bootstrap5(app)
 
 
 # CREATE DB
-class Base(DeclarativeBase):
-    pass
+# class Base(DeclarativeBase):
+#     pass
 
-db = SQLAlchemy(model_class=Base)
+# db = SQLAlchemy(model_class=Base)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI")
 
 uri = app.config['SQLALCHEMY_DATABASE_URI']
@@ -35,24 +38,122 @@ app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 # CREATE TABLE
-class Movie(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    year: Mapped[int] = mapped_column(Integer, nullable=False)
-    description: Mapped[str] = mapped_column(String(500), nullable=False)
-    rating: Mapped[float] = mapped_column(Float, nullable=True)
-    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
-    review: Mapped[str] = mapped_column(String(250), nullable=True)
-    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+# class Movie(db.Model):
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+#     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+#     year: Mapped[int] = mapped_column(Integer, nullable=False)
+#     description: Mapped[str] = mapped_column(String(500), nullable=False)
+#     rating: Mapped[float] = mapped_column(Float, nullable=True)
+#     ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+#     review: Mapped[str] = mapped_column(String(250), nullable=True)
+#     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 with app.app_context():
     db.create_all()
 
+
+# Create an admin-only decorator
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # If id is not 1 then return abort with 403 error
+        if current_user.id != 1:
+            return abort(403)
+        # Otherwise continue with the route function
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+def is_strong_password(password):
+    pattern = r"^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$"
+    return re.match(pattern, password)
+
+# Registration
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if not is_strong_password(password):
+            flash(
+                "❌ Password must be at least 8 characters long and include "
+                "one uppercase letter, one number, and one special character.",
+                "danger"
+            )
+            return redirect(url_for("register"))
+
+
+        if User.query.filter_by(email=email).first():
+            flash("❌ Email already registered. Please login.", "danger")
+            return redirect(url_for("register"))
+
+        hashed_password = generate_password_hash(password)
+
+        user = User(
+            name=name,
+            email=email,
+            password=hashed_password,
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash("✅ Registration successful! Please login.", "success")
+        return redirect(url_for("login"))
+
+    return render_template("register.html")
+
+
+# Login Route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.password and check_password_hash(user.password, password):
+            login_user(user)
+            flash("✅ Successfully logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("❌ Invalid email or password.", "danger")
+
+    return render_template("login.html")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash(" 👋 You have been logged out successfully.", "info")
+    return redirect('/index')
+
 class RateMovieForm(FlaskForm):
-    rating = StringField('Your Rating Out of 10 e.g. 7.5')
-    review = StringField('Your Review')
-    submit = SubmitField('Done')
+    rating = FloatField(
+        "Your Rating",
+        validators=[
+            DataRequired(),
+            NumberRange(min=0, max=10)
+        ]
+    )
+    review = StringField(
+        "Your Review",
+        validators=[DataRequired()]
+    )
+    submit = SubmitField("Done")
 
 
 class FindMovieForm(FlaskForm):
@@ -89,8 +190,10 @@ class FindMovieForm(FlaskForm):
 
 
 @app.route("/")
+@app.route("/index")
+@login_required
 def home():
-    result = db.session.execute(db.select(Movie).order_by(Movie.rating))
+    result = db.session.execute(db.select(Movie).where(Movie.user_id == current_user.id).order_by(Movie.rating))
     all_movies = result.scalars().all() #converts ScalarResult to Python list
 
     for i in range(len(all_movies)):
@@ -100,6 +203,7 @@ def home():
     return render_template("index.html", movies=all_movies)
 
 @app.route("/add", methods=['GET', 'POST'])
+@login_required
 def add_movie():
     form = FindMovieForm()
     if form.validate_on_submit():
@@ -110,6 +214,7 @@ def add_movie():
     return render_template('add.html', form=form)
 
 @app.route("/find")
+@login_required
 def find_movie():
     movie_api_id = request.args.get('id')
     if movie_api_id:
@@ -120,7 +225,8 @@ def find_movie():
             title=data["title"],
             year=data["release_date"].split("-")[0],
             img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
-            description=data["overview"]
+            description=data["overview"],
+            user_id=current_user.id
         )
         db.session.add(new_movie)
         db.session.commit()
@@ -129,18 +235,20 @@ def find_movie():
 
 
 @app.route("/edit", methods=['GET', 'POST'])
+@login_required
 def rate_movie():
     form = RateMovieForm()
     movie_id = request.args.get('id')
     movie = db.get_or_404(Movie, movie_id)
     if form.validate_on_submit():
-        movie.rating = float(form.rating.data)
+        movie.rating = form.rating.data
         movie.review = form.review.data
         db.session.commit()
         return redirect(url_for('home'))
     return render_template('edit.html', movie=movie, form=form)
 
 @app.route("/delete")
+@login_required
 def delete_movie():
     movie_id = request.args.get('id')
     movie_to_delete = db.get_or_404(Movie, movie_id)
@@ -149,7 +257,17 @@ def delete_movie():
 
     return redirect(url_for('home'))
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/developer")
+def developer():
+    return render_template('developer.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
